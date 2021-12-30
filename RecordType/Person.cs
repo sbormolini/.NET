22d1cbb8d66deb3c5ea05958af5ab0908eb8e10c
Sandro Bormolini <sandro.bormolini@outlook.com>
2021-12-30 22:17:40 +0100

    Add RecordType

---
+++ RecordType/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordType;
+
+public record Person(int Id, string FullName, DateOnly DateOfBirth);
+
+//public record class Person(int Id, string FullName, DateOnly DateOfBirth);
+
+//public record Person
+//{
+//    public int Id { get; init; } = default!;
+//    public string FullName { get; init; } = default!;
+//    public DateOnly DateOfBirth { get; init; } = default!;
+
+//    protected virtual bool PrintMembers(StringBuilder builder)
+//    {
+//        builder.Append($"Id is: {Id}");
+//        builder.Append($"FullName is: {FullName}");
+//        builder.Append($"Date of Birth is: {DateOfBirth}");
+//        return true;
+//    }
+//}
+
+public record struct PersonAsStruct(int Id, string FullName, DateOnly DateOfBirth);
+
+public class PersonAsClass
+{
+    public int Id { get; init; } = default!;
+    public string FullName { get; init; } = default!;
+    public DateOnly DateOfBirth { get; init; } = default!;
+}
---
+++ RecordType/Program.cs
@@ -0,0 +1,40 @@
+﻿using RecordType;
+
+
+// records
+Person sandro = new(1, "Sandro Bormolini", new DateOnly(1986, 12, 2));
+Person sandro2 = new(1, "Sandro Bormolini", new DateOnly(1986, 12, 2));
+
+Person sandroButYounger = sandro with { DateOfBirth = new DateOnly(1996, 12, 2) };
+
+Console.WriteLine(sandro);
+Console.WriteLine(sandroButYounger);
+// compare
+Console.WriteLine(sandro == sandro2);
+Console.WriteLine(ReferenceEquals(sandro, sandro2));
+
+// deconstruct
+var (id, name, dateOfBirth) = sandro;
+Console.WriteLine($"{name} ({id}) is born at {dateOfBirth}");
+
+
+// classes
+PersonAsClass sandroAsClass = new()
+{
+    Id = 1,
+    FullName = "Sandro Bormolini",
+    DateOfBirth = new DateOnly(1986, 12, 2)
+};
+
+PersonAsClass sandroAsClass2 = new()
+{
+    Id = 1,
+    FullName = "Sandro Bormolini",
+    DateOfBirth = new DateOnly(1986, 12, 2)
+};
+
+Console.WriteLine(sandroAsClass);
+// compare
+Console.WriteLine(sandroAsClass == sandroAsClass2);
+
+// check sharplab.io
