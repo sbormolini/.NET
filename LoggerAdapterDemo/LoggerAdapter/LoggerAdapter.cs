99095b5c4500ec5e60c6d8b7105f54fc8b8c7d95
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-01-07 21:24:00 +0100

    Add LoggerAdapterDemo

---
+++ LoggerAdapterDemo/LoggerAdapter/LoggerAdapter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LoggerAdapter;
+
+public class LoggerAdapter<T> : ILoggerAdapter<T>
+{
+    private readonly ILogger<T> _logger;
+
+    public LoggerAdapter(ILogger<T> logger) => _logger = logger;
+
+    public void LogInformation(string message)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation(message);
+    }
+
+    public void LogInformation<T0>(string message, T0 args0)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation(message + " (from adapter with <3)", args0);
+    }
+
+    public void LogInformation<T0, T1>(string message, T0 args0, T1 args1)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation(message, args0, args1);
+    }
+
+    public void LogInformation<T0, T1, T2>(string message, T0 args0, T1 args1, T2 args2)
+    {
+        if (_logger.IsEnabled(LogLevel.Information))
+            _logger.LogInformation(message, args0, args1, args2);
+    }
+}
+
+public interface ILoggerAdapter<T>
+{
+    void LogInformation(string message);
+
+    void LogInformation<T0>(string message, T0 args0);
+
+    void LogInformation<T0, T1>(string message, T0 args0, T1 args1);
+
+    void LogInformation<T0, T1, T2>(string message, T0 args0, T1 args1, T2 args2);
+}
