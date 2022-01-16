b417dbcf6786220d318bb43582073c9a6098b8cb
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-01-16 20:17:21 +0100

    Add BenchmarkDemo

---
+++ BenchMarkDemo/BenchMarker/Demo.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using System.Text;
+
+namespace BenchMarker
+{
+    //[SimpleJob(RuntimeMoniker.Net50, baseline: true)]
+    [SimpleJob(RuntimeMoniker.Net60)]
+    [MemoryDiagnoser]
+    public class Demo
+    {
+        [Benchmark(Baseline = true)]
+        public string GetFullStringNormally()
+        {
+            string output = "";
+
+            for (int i = 0; i < 100; i++)
+            {
+                output += i;
+            }
+
+            return output;
+        }
+
+        [Benchmark]
+        public string GetFullStringWithStringBuilder()
+        {
+            StringBuilder output = new();
+
+            for (int i = 0; i < 100; i++)
+            {
+                output.Append(i);
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ BenchMarkDemo/BenchMarker/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using BenchMarker;
+
+var results = BenchmarkRunner.Run<Demo>();
