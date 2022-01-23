df9d5fec8dceb79afdb6c75e5bd68cfc2662fac2
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-01-23 21:28:49 +0100

    Add MSALDemo

---
+++ MSALDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Identity.Client;
+
+// source https://docs.microsoft.com/en-us/learn/modules/implement-authentication-by-using-microsoft-authentication-library/3-initialize-client-applications
+
+var clientId = "";
+var clientSecret = "";
+
+string redirectUri = "https://myapp.azurewebsites.net";
+
+// Initializing public and confidential client applications
+IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(clientId)
+    .WithClientSecret(clientSecret)
+    .WithRedirectUri(redirectUri)
+    .Build();
+
+//// build modifiers
+//var clientApp = PublicClientApplicationBuilder.Create(client_id)
+//    .WithAuthority(AzureCloudInstance.AzurePublic, tenant_id)
+//    .Build();
+
+//var clientApp = PublicClientApplicationBuilder.Create(client_id)
+//    .WithAuthority(AzureCloudInstance.AzurePublic, tenant_id)
+//    .WithRedirectUri("http://localhost")
+//    .Build();
