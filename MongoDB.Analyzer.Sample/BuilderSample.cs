9d3824080a3cef0f3016fc4b60b777bdfc4c6978
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-01-25 20:53:40 +0100

    Add MongoDB.Analyzer.Sample

---
+++ MongoDB.Analyzer.Sample/BuilderSample.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Analyzer.Sample.Models;
+using MongoDB.Driver;
+
+namespace MongoDB.Analyzer.Sample.Basic;
+
+public class BuilderSample
+{
+    public async Task<List<Movie>> GetMovies(double minScore, Genre genre, string titleSearchTerm)
+    {
+        var mongoClient = new MongoClient(@"mongodb://localhost:27017");
+        var db = mongoClient.GetDatabase("testdb");
+        var moviesCollection = db.GetCollection<Movie>("movies");
+
+        // Filter definition (analyzer provides mql as information message)
+        var filter = Builders<Movie>.Filter.Eq(m => m.Genre, genre) &
+            Builders<Movie>.Filter.Gte(m => m.Score, minScore) &
+            Builders<Movie>.Filter.Regex(m => m.Title, titleSearchTerm);
+
+        // Sort definition (analyzer provides mql as information message)
+        var sort = Builders<Movie>.Sort.Ascending(m => m.Score)
+                                       .Descending(m => m.Title);
+
+        var movies = await moviesCollection.Find(filter).Sort(sort).ToListAsync();
+
+        return movies;
+    }
+
+    public void NotSupoortedFilter()
+    {
+        var filter = Builders<Movie>.Filter.Gt(m => m.Reviews.Length, 2);
+    }
+}
---
+++ MongoDB.Analyzer.Sample/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿namespace MongoDB.Analyzer.Sample.Models
+{
+    public enum Genre 
+    {
+        Action,
+        Drama, 
+        Comedy
+    };
+
+    public record Movie
+    {
+        public string? Title { get; set; }
+        public int Score { get; set; }
+        public Genre Genre { get; set; }
+        public string? Reviews { get; set; }
+    }
+}
