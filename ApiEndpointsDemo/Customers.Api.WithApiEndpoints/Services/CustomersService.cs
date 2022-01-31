42cf0e5dfe3a52d93aab80e680e3f36f7e340f5c
Sandro <sandro.bormolini@outlook.com>
2022-01-31 11:11:45 +0100

    Implement ICustomersService

--- ApiEndpointsDemo/Customers.Api.WithApiEndpoints/Services/CustomersService.cs
+++ ApiEndpointsDemo/Customers.Api.WithApiEndpoints/Services/CustomersService.cs
@@ -6,6 +6,10 @@ public class CustomersService : ICustomersService
 {
     private readonly Dictionary<Guid, Customer> _customers = new();
 
+    public Customer? GetById(Guid id) => _customers.GetValueOrDefault(id);
+
+    public List<Customer> GetAll() => _customers.Values.ToList();
+
     public bool Create(Customer? customer)
     {
         if (customer is null)
@@ -14,10 +18,6 @@ public class CustomersService : ICustomersService
         _customers[customer.Id] = customer;
         return true;
     }
-
-    public Customer? GetById(Guid id) => _customers.GetValueOrDefault(id);
-
-    public List<Customer> GetAll() => _customers.Values.ToList();
 
     public bool Update(Customer customer)
     {
@@ -39,17 +39,36 @@ public class CustomersService : ICustomersService
         return true;
     }
 
-    public Task<List<Customer>> GetAllAsync()
-    {
-        return Task.FromResult(_customers.Values.ToList());
-    }
+    public Task<List<Customer?>> GetAllAsync() => Task.FromResult(_customers.Values.ToList());
+    
+    public Task<Customer?> GetByIdAsync(Guid id) => Task.FromResult(_customers?.GetValueOrDefault(id));
 
     public Task<bool> CreateAsync(Customer customer)
     {
         if (customer is null)
-            return false;
+            return Task.FromResult(false);
 
         _customers[customer.Id] = customer;
-        return true;
+        return Task.FromResult(true);
+    }
+
+    public Task<bool> DeleteAsync(Guid id)
+    {
+        var existingCustomer = GetById(id);
+        if (existingCustomer is null)
+            return Task.FromResult(false);
+
+        _customers.Remove(id);
+        return Task.FromResult(true);
+    }
+
+    public Task<bool> UpdateAsync(Customer customer)
+    {
+        var existingCustomer = GetById(customer.Id);
+        if (existingCustomer is null)
+            return Task.FromResult(false);
+
+        _customers[customer.Id] = customer;
+        return Task.FromResult(true);
     }
 }
