ae0d296832991b0842fcb7b35621aca5477ae485
Sandro <sandro.bormolini@outlook.com>
2022-02-01 17:51:28 +0100

    Add CallerArgumentExpressionDemo

---
+++ CallerArgumentExpressionDemo/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CallerArgumentExpressionDemo;
+
+public class Helper
+{
+    public static void ItIsNotEmpty<T>(
+        IEnumerable<T> value,
+        [CallerArgumentExpression("value")] string message = "")
+    {
+        if (!value.Any())
+            throw new ArgumentException("Enumerable is empty", message);
+    }
+}
---
+++ CallerArgumentExpressionDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿using CallerArgumentExpressionDemo;
+using System.Diagnostics;
+
+var array = new[] { 1, 2, 3 };
+
+Debug.Assert(array.Length == 3);
+Debug.Assert(args.Length == 0, "args.Length == 0");
+
+var arrayEmpty = new int[0];
+
+Helper.ItIsNotEmpty(arrayEmpty);
