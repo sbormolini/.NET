cb63bd57bd85b57c963c2d3c3ec2d6ca60e2b239
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-02-18 22:33:51 +0100

    Added excercise AtbashCipher

---
+++ Exercism/Exercism/Exercises/AtbashCipher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism;
+
+public class AtbashCipher
+{
+    public static string Encode(string plainValue) =>
+        string.Concat(plainValue
+            .Where(char.IsLetterOrDigit)
+            .Select(char.ToLower)
+            .Select(c => char.IsLetter(c)? (char)('a' + 'z' - c) : c)
+            .SelectMany((c, index) => (index != 0) && (index % 5 == 0) ? $" {c}" : $"{c}"));
+
+    public static string Decode(string encodedValue) => 
+        string.Concat(encodedValue
+            .Where(char.IsLetterOrDigit)
+            .Select(c => char.IsLetter(c) ? (char)('a' + 'z' - c) : c));
+}
--- Exercism/Exercism/Program.cs
+++ Exercism/Exercism/Program.cs
@@ -1,15 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Exercism;
-using Xunit;
 
-var matrix = new[,]
-{
-        { 9, 8, 7 },
-        { 5, 3, 2 },
-        { 6, 6, 7 }
-};
-var actual = SaddlePoints.Calculate(matrix);
-var expected = new[] { (2, 1) };
-
-if (expected == actual)
-    Console.WriteLine("YES");
+string value = AtbashCipher.Encode("aez");
+Console.WriteLine(value);
