3182ef0a7b39aec33819266cbe2a1be5c06bdc00
Sandro Bormolini <sandro.bormolini@itnetx.ch>
2022-02-18 17:55:59 +0100

    Add FlattenArray

---
+++ Exercism/Exercism/Exercises/FlattenArray.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace Exercism.Exercises;
+
+public class FlattenArray
+{
+    public static IEnumerable Flatten(IEnumerable input)
+    {
+        foreach (var i in input)
+        {
+            if (i is IEnumerable enumerable)
+            {
+                foreach (var j in Flatten(enumerable))
+                    yield return j;
+            }
+            else if (i is not null)
+                yield return i;
+        }
+    }
+}
