0fc85e79660eea584d0d40d66240719e8dd79f61
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-03-07 15:38:19 +0100

    Added exercise IsbnVerifier

---
+++ Exercism/Exercism/Exercises/IsbnVerifier.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercism.Exercises;
+
+public static class IsbnVerifier
+{
+    private const string IsbnPattern = @"^\d(-?)\d{3}\1\d{5}\1[\dX]$";
+
+    public static bool IsValid(string number) => 
+        IsIsbnPattern(number) && IsValidIsbnCheckSum(number);
+
+    private static bool IsIsbnPattern(string number) => 
+        Regex.IsMatch(number, IsbnPattern);
+
+    private static bool IsValidIsbnCheckSum(string number) => 
+        GetIsbnCheckSum(number) % 11 == 0;
+   
+    private static int GetIsbnCheckSum(string number) => 
+        IsbnDigits(number).Select((x, i) => x * (10 - i)).Sum();
+
+    private static IEnumerable<int> IsbnDigits(string number) => 
+        number.Where(char.IsLetterOrDigit).Select(x => x == 'X' ? 10 : int.Parse(x.ToString()));
+}
