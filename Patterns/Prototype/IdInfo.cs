fe662d40d0d730eaea7985969c66275bd7603f83
Sandro Bormolini <sandro.bormolini@itnetx.ch>
2022-03-22 11:33:50 +0100

    Added Prototype pattern

---
+++ Patterns/Prototype/IdInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Prototype;
+
+public class IdInfo
+{
+    public int IdNumber;
+
+    public IdInfo(int idNumber)
+    {
+        IdNumber = idNumber;
+    }
+}
