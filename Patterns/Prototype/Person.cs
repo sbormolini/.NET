c156724ac5ad6e24d950778d9135f8e0967facea
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-03-22 11:45:06 +0100

    Added Prototype pattern

---
+++ Patterns/Prototype/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Prototype;
+
+public class Person
+{
+    public int Age;
+    public DateTime BirthDate;
+    public string Name;
+    public IdInfo IdInfo;
+
+    public Person ShallowCopy()
+    {
+        return (Person)MemberwiseClone();
+    }
+
+    public Person DeepCopy()
+    {
+        Person clone = (Person)MemberwiseClone();
+        clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+        clone.Name = String.Copy(Name);
+        return clone;
+    }
+}
