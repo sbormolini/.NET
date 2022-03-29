9d1a497810eb6b98bb87d875c82301b9eed1550d
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-03-29 14:20:23 +0200

    Added Observer pattern

---
+++ Patterns/Observer/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://refactoring.guru/design-patterns/observer/csharp/example#lang-features for more information
+
+using DesignPatterns.Observer;
+
+// The client code.
+var subject = new Subject();
+var observerA = new ConcreteObserverA();
+subject.Attach(observerA);
+
+var observerB = new ConcreteObserverB();
+subject.Attach(observerB);
+
+subject.SomeBusinessLogic();
+subject.SomeBusinessLogic();
+
+subject.Detach(observerB);
+
+subject.SomeBusinessLogic();
