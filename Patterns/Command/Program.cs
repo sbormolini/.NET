d1a31d169ba717c0c13e06124a342f0ede98d882
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-03-30 10:39:29 +0200

    Added Command pattern

---
+++ Patterns/Command/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DesignPatterns.Command;
+
+// The client code can parameterize an invoker with any commands.
+Invoker invoker = new();
+invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+Receiver receiver = new();
+invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
+
+invoker.DoSomethingImportant();
