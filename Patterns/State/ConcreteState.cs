8bc7a8fb17e7d196f5bacdfabba30d5e4d80b3a1
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-03-31 17:08:11 +0200

    Added State pattern

---
+++ Patterns/State/ConcreteState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.State;
+
+// Concrete States implement various behaviors, associated with a state of
+// the Context.
+class ConcreteStateA : State
+{
+    public override void Handle1()
+    {
+        Console.WriteLine("ConcreteStateA handles request1.");
+        Console.WriteLine("ConcreteStateA wants to change the state of the context.");
+        _context.TransitionTo(new ConcreteStateB());
+    }
+
+    public override void Handle2()
+    {
+        Console.WriteLine("ConcreteStateA handles request2.");
+    }
+}
+
+class ConcreteStateB : State
+{
+    public override void Handle1()
+    {
+        Console.Write("ConcreteStateB handles request1.");
+    }
+
+    public override void Handle2()
+    {
+        Console.WriteLine("ConcreteStateB handles request2.");
+        Console.WriteLine("ConcreteStateB wants to change the state of the context.");
+        _context.TransitionTo(new ConcreteStateA());
+    }
+}
---
+++ Patterns/State/State.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.State;
+
+// The base State class declares methods that all Concrete State should
+// implement and also provides a backreference to the Context object,
+// associated with the State. This backreference can be used by States to
+// transition the Context to another State.
+abstract class State
+{
+    protected Context _context;
+
+    public void SetContext(Context context) => _context = context;
+
+    public abstract void Handle1();
+
+    public abstract void Handle2();
+}
