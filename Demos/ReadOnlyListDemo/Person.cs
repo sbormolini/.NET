efa1557760749041673f5bc5e4a5a91374b843a3
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-05-03 17:07:34 +0200

    Added ReadOnlyListDemo

---
+++ Demos/ReadOnlyListDemo/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyListDemo;
+
+internal class Person
+{
+    private readonly List<Person> _children;
+
+    public string Name { get; set; }
+    //public IEnumerable<Person> Children { get => _children; }
+    public IReadOnlyCollection<Person> Children { get => _children.AsReadOnly(); }
+
+    public Person(string name)
+    {
+        Name = name;
+        _children = new List<Person>();
+    }
+
+    public void AddChild(Person child)
+    {
+        Console.WriteLine($"Congratulations! {Name} became a parent for {child.Name}");
+        _children.Add(child);
+    }
+
+    public void PrintChildren()
+    {
+        if (_children.Count > 0) 
+            Console.WriteLine($"{Name}'s children: ");
+
+        foreach (var child in _children)
+        {
+            Console.WriteLine(child.Name);
+            child.PrintChildren();
+        }
+    }
+}
---
+++ Demos/ReadOnlyListDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ReadOnlyListDemo;
+
+Person peter = new("Peter");
+Person john = new("John");
+
+peter.AddChild(john);
+
+//// access list without read only list
+//IList<Person>? petersChildren = peter.Children as IList<Person>;
+//if (petersChildren != null)
+//    petersChildren.Add(new Person("Child X"));
+
+peter.PrintChildren();
