7ec8374abecdd444b79ea22afb1519c4f036edc1
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-06-19 20:57:54 +0200

    Added EnvironmentVariableDemo

---
+++ Demos/EnvironmentVariableDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EnvironmentVariableDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var environment = Environment.GetEnvironmentVariable("test");
+        Console.WriteLine("test var: " + environment);
+    }
+}
